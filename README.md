649cbb5339a3aa13531880d1da3481522e695c45
Shoaib Sharif <sharifshoaib@yahoo.com>
2020-04-24 20:23:28 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quizzler ❓
+
+A flutter quiz App.
---
+++ lib/quiz.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Quiz {
+  String question;
+  bool answer;
+
+  Quiz({@required this.question, @required this.answer});
+}
